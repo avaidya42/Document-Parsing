b98054adc66451f94dbf24f29d3b70f25c507040
mshibashish <shibashish.mahapatra@kare4u.in>
2025-06-23 03:23:39 +0530

    duplicate handling and result merging in magma_parser.py

--- magma_parser.py
+++ magma_parser.py
@@ -2,6 +2,7 @@ from parsing_utils import table_loader_multiple
 import pandas as pd
 import json
 from utils import text_space_cleaner
+from collections import Counter
 
 
 pd.set_option('display.max_rows', None)
@@ -16,31 +17,50 @@ file_path = "C:\Kare4U\document_parsing\other_documents\Magma HDI General Insura
 
 dfs = table_loader_multiple(file_path, "Policy Schedule /TAX INVOICE", "IN WITNESS WHEREOF")
 
+def convert_to_dict(df, result, key_counts, include_headers=False):
+    def repeat_checker(key_counts, key):
+        if key_counts[key] > 1:
+            final_key = f"{key}_{key_counts[key]}"
+        else:
+            final_key = key
+        return final_key
+
+    if include_headers:
+        cols = df.columns.tolist()
+        if len(cols) >= 2:
+            header_key = text_space_cleaner(cols[0])
+            key_counts[header_key] += 1
+
+            result[repeat_checker(key_counts, header_key)] = cols[1]
+
+    for key, value in zip(df.iloc[:, 0], df.iloc[:, 1]):
+        if pd.notna(key) and pd.notna(value):
+            cleaned_key = text_space_cleaner(key)
+            key_counts[cleaned_key] += 1
+
+            # Add number suffix for duplicates
+
+            result[repeat_checker(key_counts, cleaned_key)] = value
+    # return result
+
+key_counts = Counter()
+result = {}
+
 next_df = False
 for df in dfs:
     # print(df)
     cols = df.columns.tolist()
     if 'Policy Details' in cols:
-        result = df.set_index(df.columns[0])[df.columns[1]].to_dict()
+        extra_result = df.set_index(df.columns[0])[df.columns[1]].to_dict()
     elif "Cover" in cols and "Coverage Details" in cols:
         # print(df, cols)
         next_df = True
-        result2 = {
-            text_space_cleaner(key): value
-            for key, value in zip(df.iloc[:, 0], df.iloc[:, 1])
-            # if pd.notna(key) and pd.notna(value)
-        }
+        convert_to_dict(df, result, key_counts)
         # continue
     elif next_df:
         next_df = False
-        result3 = {cols[0]: cols[1]}
-        result3.update({
-            text_space_cleaner(key): value
-            for key, value in zip(df.iloc[:, 0], df.iloc[:, 1])
-            # if pd.notna(key) and pd.notna(value)
-        })
+        convert_to_dict(df, result, key_counts, include_headers=True)
 
 print(json.dumps(result, indent=4))
-print(json.dumps(result2, indent=4))
-print(json.dumps(result3, indent=4))
+print(json.dumps(extra_result, indent=4))
 
